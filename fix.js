1504627991f0d99e01f921856e8b6912856f62cc
Evan <avjet.ai@gmail.com>
2026-08-11 13:57:58 +0800

    清理專案：刪除根目錄不必要的暫存腳本與發佈紀錄草稿檔

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/substyle.js', 'utf8');
-code = code.replace(/    \}\r?\n  \} else \{/g, '  } else {');
-fs.writeFileSync('src/substyle.js', code);
--- test_css.js
+++
@@ -1,3 +0,0 @@
-import { styleToCss, effStyle } from './src/substyle.js';
-const st = effStyle({ style: { bgBox: true, bgColor: '#ff0000', bgAlpha: 0.8, shadow: 2 } });
-console.log(styleToCss(st));
